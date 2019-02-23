d2f6b6bee7ca94d48fb7592763b2c2ae27900961
LycorisQi <47167828+LycorisQi@users.noreply.github.com>
2019-02-23 14:43:54 -0800

    Create pointQuick.js

---
+++ pointQuick.js
@@ -0,0 +1,17 @@
+function setup() 
+{
+  createCanvas(400, 400);
+  colorMode(HSB, width, height, 100);
+}
+
+function draw() 
+{
+  var x = random(width);
+  var y = random(height);
+	
+  for(i = 0; i < 100; i++)
+  {
+    stroke(x, y, 100);
+ 		point(x, y);
+  }
+}
